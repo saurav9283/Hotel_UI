91e0981e25dc962f2227b6765f997a92116e2e81
Saurav <sauravkumar00108@gmail.com>
2023-10-16 23:52:36 +0530

    changes in context api

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,47 @@
-import { Link, useNavigate } from "react-router-dom"
-import "./navbar.css"
-import { AuthContext } from "../context/AuthContext"
-import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom";
+import "./navbar.css";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 function Navbar() {
   const navigate = useNavigate();
-  const {user} = useContext(AuthContext)
+  const { user, logout } = useContext(AuthContext);
+  const { dispatch } = useContext(AuthContext);
+
 
   const goLogin = () => {
-    navigate("/Login")
+    navigate("/Login");
+  };
+
+  const handleLogout = () => {
+    // Perform the logout action
+    dispatch({ type: "LOGOUT" });
   }
 
   return (
     <div className="navbar">
       <div className="navcontainer">
-      <Link to="/" style={{color:"inherit" , textDecoration:"none"}}>
-        <span className="logo">Lamabooking</span>
-      </Link>
-        {user ? user.username :(<div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton" onClick={goLogin}>Login</button>
-        </div>)}
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Lamabooking</span>
+        </Link>
+        {user ? (
+          <div className="user">
+            <div>
+              <button onClick={handleLogout} className="logout">Logout</button>
+            </div>
+            {user.username}
+          </div>
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton" onClick={goLogin}>
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
