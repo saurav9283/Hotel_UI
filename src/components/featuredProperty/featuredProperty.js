cb9f747246ca7c76e699bfe06b0bd4bd2ee8663c
Saurav <sauravkumar00108@gmail.com>
2023-10-17 23:58:14 +0530

    changes

--- src/components/featuredProperty/featuredProperty.js
+++ src/components/featuredProperty/featuredProperty.js
@@ -5,7 +5,7 @@ import useFetch from "../hooks/useFetch";
 
 function FeaturedProperty() {
   const { data, loading, error } = useFetch(
-    "https://hotel-management-api.vercel.app/api/hotels?featured=true"
+    "https://hotel-management-api.vercel.app/api/hotels?featured=true&limit=2"
     // "http://localhost:8000/api/hotels?featured=true&limit=2"
   );
 
@@ -15,21 +15,23 @@ function FeaturedProperty() {
         "Loading"
       ) : (
         <>
-          {
-            data.map((item) => (
-              <div className="fpItem" key={item._id}>
-            {/* <img src={item.photos[0]} alt="" className="fpImg"></img> */}
-            <img src={hotel} alt="" className="fpImg"></img>
-            <span className="fpname">{item.name}</span>
-            <span className="fpCity">{item.city}</span>
-            <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-            {item.rating && 
-            <div className="fpRating">
-              <button className="fpButton">{item.rating}</button>
-              <span>Excillent</span>
-            </div>}
-          </div>
-      ))}
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              {/* <img src={item.photos[0]} alt="" className="fpImg"></img> */}
+              <img src={hotel} alt="" className="fpImg"></img>
+              <span className="fpname">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">
+                Starting from ${item.cheapestPrice}
+              </span>
+              {item.rating && (
+                <div className="fpRating">
+                  <button className="fpButton">{item.rating}</button>
+                  <span>Excillent</span>
+                </div>
+              )}
+            </div>
+          ))}
         </>
       )}
     </div>
